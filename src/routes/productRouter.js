d2906a5f3c3f4e06c818966e5dde610d469f091e
Yesika Perez Ravelo <yesikapr@gmail.com>
2024-04-14 23:34:18 -0300

    paginate

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -9,12 +9,35 @@ const router = Router();
 const ProductService = new productManagerDB();
 
 router.get("/", async (req, res) => {
-  const result = await ProductService.getAllProducts();
-
-  res.send({
-    status: "success",
-    payload: result,
-  });
+  try {
+    let { limit = 10, page = 1, query = {}, sort = null } = req.query;
+    const result = await ProductService.getAllProducts(
+      limit,
+      page,
+      query,
+      sort
+    );
+    res.render("products", {
+      layout: "default",
+      style: "index.css",
+      status: "success",
+      products: result.docs,
+      totalPages: result.totalPages,
+      prevPage: result.prevPage,
+      nextPage: result.nextPage,
+      page: result.page,
+      hasPrevPage: result.hasPrevPage,
+      hasNextPage: result.hasNextPage,
+      prevLink: result.prevPage
+        ? `http://localhost:8080/products?page=${result.prevPage}`
+        : null,
+      nextLink: result.nextPage
+        ? `http://localhost:8080/products?page=${result.nextPage}`
+        : null,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 router.post("/", uploader.array("thumbnails", 3), async (req, res) => {
